dfbb04216c299e3040d59dbf596483b4d7e4e631
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-06-19 22:16:13 -0300

    Primeira aula

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+main() => runApp(const PerguntaApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
+
+  void responder() {
+    print('Pergunta respondida!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Perguntas'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Perguntas"),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text(perguntas[0]),
+              ElevatedButton(
+                onPressed: responder,
+                child: const Text('Resposta 1'),
+              ),
+              ElevatedButton(
+                onPressed: responder,
+                child: const Text('Resposta 3'),
+              ),
+              ElevatedButton(
+                onPressed: responder,
+                child: const Text('Resposta 3'),
+              ),
+            ],
+          )),
     );
   }
 }
