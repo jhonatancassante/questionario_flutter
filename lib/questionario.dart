24f3d388cd5d0aa944e02d44fc6c5d8582ecbe35
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-06-24 09:04:38 -0300

    Implementação de notas nas respostas

--- lib/questionario.dart
+++ lib/questionario.dart
@@ -20,14 +20,17 @@ class Questionario extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<String> respostas = temPerguntaSelecionada
-        ? perguntas[perguntaSelecionada].cast()['respostas']
+    List<Map<String, Object>> respostas = temPerguntaSelecionada
+        ? perguntas[perguntaSelecionada]['respostas']
+            as List<Map<String, Object>>
         : [];
 
     return Column(
       children: <Widget>[
         Questao(perguntas[perguntaSelecionada]['texto'].toString()),
-        ...respostas.map((e) => Resposta(e, responder)).toList(),
+        ...respostas
+            .map((resp) => Resposta(resp['texto'] as String, responder))
+            .toList(),
       ],
     );
   }
